4482320eb534488f6ba1f5d3ce58599bd3d4be19
CHANMOON PARK <thechanmoon@gmail.com>
2020-02-14 14:22:43 +0000

    Automatically backed up by Learn

--- spec/nds_extract_spec.rb
+++ spec/nds_extract_spec.rb
@@ -1,4 +1,4 @@
-require 'spec_helper'
+learrequire 'spec_helper'
 
 describe 'The directors_database method can be processed by the directors_totals method' do
   it 'which returns a Hash describing director to total' do
